45b84e001779811fdb83eb867892f59ed3997243
sama-babaee-web <samababaee8866@gmail.com>
2023-11-09 15:02:05 +0330

    create and design home page

--- pages/Components/Header/index.jsx
+++ pages/Components/Header/index.jsx
@@ -4,46 +4,42 @@ import styles from './header.module.css'
 function Header() {
     return (
         <>
-            <header className={`${styles["bg-head"]} col-12 container-fluid`}>
-                <nav className="navbar">
-                    <div className="container-fluid ">
+            <header className="col-12 m-0 ">
+                <nav className="navbar navbar-expand-lg headerNav__sama">
+                    <div className="container-fluid">
                         <Link href='/' className="navbar-brand">
                             <Image
                                 src="/img/logo.png"
-                                width={80}
+                                width={180}
                                 height={80}
                                 alt="سما بابایی"
+                                priority
                             />
                         </Link>
-                        <nav className="navbar navbar-expand-lg ">
-                                    <div className="container-fluid">
-                                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                            <span className="navbar-toggler-icon"></span>
-                                        </button>
-                                        <div className="collapse navbar-collapse" id="navbarNav">
-                                            <ul className="navbar-nav">
-                                                <li className={` ${styles["cus-box"]} nav-item m-2  p-1 py-2 rounded-3 `}>
-                                                    <a className="nav-link active" aria-current="page" href="#">Home</a>
-                                                </li>
-                                                <li className={` ${styles["cus-box"]} nav-item m-2  py-2 rounded-3 `}>
-                                                    <a className="nav-link" href="#">Features</a>
-                                                </li>
-                                                <li className={` ${styles["cus-box"]} nav-item m-2  py-2 rounded-3 `}>
-                                                    <a className="nav-link" href="#">Pricing</a>
-                                                </li>
-                                                <li className={` ${styles["cus-box"]} nav-item m-2  py-2 rounded-3 `}>
-                                                    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </nav>
+                        <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="navbar-collapse justify-content-end align-items-center collapse" id="navbarNavAltMarkup">
+                            <div className="navbar-nav d-flex align-items-center">
+                                <ul className="navbar-nav text-center">
+                                    <li className="nav-item px-4">
+                                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                    </li>
+                                    <li className="nav-item px-4">
+                                        <a className="nav-link" href="#">Features </a>
+                                    </li>
+                                    <li className="nav-item px-4">
+                                        <a className="nav-link" href="#">Pricing</a>
+                                    </li>
+                                    <li className="nav-item px-4">
+                                        <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    
-                </nav>
-                <div className={styles["line-bottom"]}></div>
-
-                <div className={styles["hero-background-top"]}></div>
+                </nav>  
+                <div className={styles["line-bottom-header"]}></div>
             </header>
         </>
     )
--- pages/Components/Home/index.jsx
+++ pages/Components/Home/index.jsx
@@ -4,13 +4,14 @@ import { BiRadioCircle } from "react-icons/bi";
 function HomeWrapper() {
     return (
         <>
-            <section className={`${styles["wrap"]} container my-5 `} >
-                <div className={`${styles[""]} row m-0 col-12 h-auto`}>
+            <section className="my-5">
+                <div className="row m-0 col-12 ">
 
-                    <div className="col-12 col-lg-4  my-2" >
+                    {/* Left side section Home */}
+                    <div className="col-12 col-lg-4 my-2 p-0 m-0" >
 
-                        <div className={styles["cus-box"]}>
-                            <Image className={`${styles["cus-bdrs", 'ss']} img-fluid col-12 p-0 m-0 w-100`}
+                        <div className='pe-lg-4'>
+                            <Image className={`${styles["cus-bdrs" ,"cus-box"]} img-fluid col-12 p-0 m-0 w-100`}
                                 src="/img/sama.png"
                                 width={500}
                                 height={500}
@@ -19,17 +20,19 @@ function HomeWrapper() {
                         </div>
 
                     </div>
+                    {/* Left side section Home */}
 
 
 
 
-                    <div className="col-12 col-lg-8 m-0 ">
+                    {/* Right side section Home */}
+                    <div className="col-12 col-lg-8 m-0 p-0">
 
-
+                        {/* Top sec */}
                         <div className="col-12 m-0 p-0 d-flex py-2 row ">
 
 
-                            <div className={` ${styles["cus-box--"]} col-12 col-lg-7  `}>
+                            <div className="col-12 col-lg-7 m-0 p-0">
                                 <div className={` ${styles["cus-box"]} py-3`}>
                                     <div className="ps-4 ">
                                         <p className={` ${styles["cus-box"]} w-50 p-2`}>  <BiRadioCircle /> Sama Babaee  </p>
@@ -41,22 +44,13 @@ function HomeWrapper() {
                                                 <h1>Hi, I’m Sama Babaee</h1>
                                                 <p>im currently 21 years old. I live in Tehran And love programming. I started my web design training in Parnian institution by Mr. Parsa Ghorbanian .</p>
                                             </div>
-
-                                            {/* <div className="p-0 m-0 d-flex justify-content-center col-12">
-                                                <Image className={` ${styles["img-website"]} img-fluid `}
-                                                    src="/img/website.jpg"
-                                                    width={200}
-                                                    height={200}
-                                                    alt="سما بابایی"
-                                                />
-                                            </div> */}
                                         </div>
                                     </div>
 
                                 </div>
                             </div>
 
-                            <div className={` ${styles["cus-box--"]} col-12 col-lg-5 ps-lg-4 py-4 py-lg-0 `}>
+                            <div className="col-12 col-lg-5 ps-lg-4 py-4 py-lg-0 m-0 p-0">
                                 <div className={` ${styles["cus-box"]} py-3 `}>
                                     <div className=" ps-4 ">
                                         <p className={` ${styles["cus-box"]} w-50 p-2`}>  <BiRadioCircle /> CONTACT   </p>
@@ -82,7 +76,12 @@ function HomeWrapper() {
                                 </div>
                             </div>
                         </div>
+                        {/* Top sec */}
 
+
+
+
+                        {/* Bottom sec */}
                         <div className="col-12 m-0 p-0 d-flex justify-content-end pb-5 mb-2">
 
                             <div className={` ${styles["cus-box"]} col-auto  py-3 my-2 col-12`}>
@@ -112,12 +111,14 @@ function HomeWrapper() {
                                         />
                                     </div>
                                 </div>
-
-
                             </div>
                         </div>
+                        {/* Bottom sec */}
+
 
                     </div>
+
+                    {/* Right side section Home */}
                 </div>
             </section>
         </>
